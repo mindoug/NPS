03c213c77514d37be86f9c53ce5242b0c713a049
Mindy Douglas <mindy.douglas@icloud.com>
2023-04-29 12:03:54 -0500

    Initial Commit

---
+++ NPS/Park.swift
@@ -0,0 +1,41 @@
+//
+//  Park.swift
+//  NPS
+//
+//  Created by Mindy Douglas on 4/29/23.
+//
+
+import Foundation
+
+struct Park {
+    var name: String
+    var designation: String
+    var description: String
+    var state: String
+    var image: [Image]
+    var address: [Address]
+    
+    static func loadParkData() {
+        
+    }
+}
+
+struct Image {
+    var image: String
+    var credit: String
+    var title: String
+    var altText: String
+    var caption: String
+}
+
+struct Address {
+    var city: String
+    var state: String
+    var type: AddressType
+}
+
+enum AddressType {
+    case physical
+    case mailing
+}
+
