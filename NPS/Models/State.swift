1cec31ca55ba6517684170f2eac81757b631ed2f
David Douglas <dave@softwaresmoothie.com>
2023-04-29 14:14:24 -0500

    fixed issue with park method, created models folder, added a State struct

---
+++ NPS/Models/State.swift
@@ -0,0 +1,14 @@
+//
+//  State.swift
+//  NPS
+//
+//  Created by David Douglas on 4/29/23.
+//
+
+import Foundation
+
+struct State {
+    var name: String
+    var abbreviation: String
+    var image: Image
+}
